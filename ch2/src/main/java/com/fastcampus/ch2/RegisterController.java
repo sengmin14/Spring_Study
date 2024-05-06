f792f3e4bb2b8a91b3334fd2cdf1df03c2230545
진성민 <sengmin14@naver.com>
2024-05-06 16:05:15 +0900

    feat : getMapping, postMapping 연습

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,19 +1,38 @@
 package com.fastcampus.ch2;
 
+import org.apache.catalina.util.URLEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@Controller // ctrl+shift+o 자동 임포트 
 public class RegisterController {
-
-	@RequestMapping("/register/add")
-	public String register()
-	{
-		return "registerForm"; // WEB-INF/views/registerForm.jsp
+//	@RequestMapping(value="/register/add", method=RequestMethod.GET) // 신규회원 가입
+	@GetMapping("/register/add") // 4.3부터 추가
+	public String register() {
+		return "registerForm";  // WEB-INF/views/registerForm.jsp
 	}
 	
-	@RequestMapping("/register/save")
-	public String save() {
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST) // 신규회원 가입
+// 	@PostMapping("/register/save")
+	@PostMapping("/register/add")
+	public String save(@ModelAttribute("user") User user, Model m) throws Exception {
+		if(!isValid(user)) {
+			String msg = "id를 잘못입력하셨습니다.";
+			
+			m.addAttribute("msg", msg);
+			return "redirect:/register/add"; // 신규회원 가입화면으로 이동(redirect)
+		}
+		
 		return "registerInfo";
 	}
+
+	private boolean isValid(User user) {
+		return false;
+	}
+	
 }
